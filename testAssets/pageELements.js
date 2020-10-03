2ccdb1bf211e2c21d4587e46eec44b70136180e9
evelynlimanjaya <crimson.eve@gmail.com>
2020-10-03 13:59:17 -0700

    DevMountain QA Course Solo Project

---
+++ testAssets/pageELements.js
@@ -0,0 +1,86 @@
+module.exports=[
+    {
+        name: 'Serious Eats Logo',
+        selector: '@seriousLogo'
+    },
+    {
+        name:'Recipes Navigation',
+        selector:'@recipesNav'
+    },
+    {
+        name:'How-to Navigation',
+        selector:'@howToNav'
+    },
+    {
+        name:'Products Navigation',
+        selector:'@productsNav'
+    },
+    {
+        name:'Culture Navigation',
+        selector:'@cultureNav'
+    },
+    {
+        name:'Dining Out Navigation',
+        selector:'@diningNav'
+    },
+    {
+        name:'grillingNav',
+        selector:'@grillingNav'
+    },
+    {
+        name:'Account Icon',
+        selector:'@accountIcon'
+    },
+    {
+        name:'Search Icon',
+        selector:'@searchIcon'
+    },
+    {
+        name:'Featured Recipes',
+        selector:'@featuredRec'
+    },
+    {
+        name:'Section Title',
+        selector:'@sectionTitle'
+    },
+    {
+        name:'Section Recipe Cards',
+        selector:'@sectionCards'
+    },
+    {
+        name:'More Button',
+        selector:'@moreBtn'
+    },
+    {
+        name:'Cocktail Title',
+        selector:'@cocktailTitle'
+    },
+    {
+        name:'Cocktail Button',
+        selector:'@cocktailBtn'
+    },
+    {
+        name:'Videos Title',
+        selector:'@videosTitle'
+    },
+    {
+        name:'Podcast Title',
+        selector:'@podcastTitle'
+    },
+    {
+        name:'Serious Eats Bottom Logo',
+        selector:'@bottomLogo'
+    },
+    {
+        name:'About Links',
+        selector:'@aboutLinks'
+    },
+    {
+        name:'Social Links',
+        selector:'@socialLinks'
+    },
+    {
+        name:'Utility Links',
+        selector:'@utilityLinks'
+    }
+]
---
+++ testAssets/sectionTitle.js
@@ -0,0 +1,10 @@
+module.exports=[
+    'The Latest',
+    'Recipes',
+    'How-Tos',
+    'Whats Popular',
+    'Culture',
+    'Product Recs',
+    'Recipe Collections',
+    'The Food Lab'    
+]
