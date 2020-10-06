5c8517d53f66e7e0b9ba61dce0c07da031b7dfb8
evelynlimanjaya <crimson.eve@gmail.com>
2020-10-06 14:47:39 -0700

    DevMountain QA Course Solo Project

--- testAssets/shopNavigationFunction.js
+++ testAssets/shopNavigationFunction.js
@@ -3,7 +3,7 @@ module.exports=(browser)=>{
             .useXpath()
             .moveToElement('//*[@class="Nav--desktop"]//span[contains(text(),"Product Recs")]', 5, 5)
             .click('//*[@class="Nav--desktop"]//*[contains(text(),"Shop")]')
-            for(let i=1;i<4;i++){
+            for(let i=1;i<10;i++){
                 browser
                     .getText(`(//*[@class="c-shopify-product-tile__title"])[${i}]`,result=>{
                         console.log(result.value)
@@ -13,6 +13,7 @@ module.exports=(browser)=>{
                         var replaceProductName=productNameString.replace(/,/g,'|')
                         var productRegExp=RegExp(replaceProductName,'i')
                         browser
+                            .moveToElement(`(//*[@class="c-shopify-product-tile__flipper"])[${i+3}]`,5,5)
                             .click(`(//*[@class="c-shopify-product-tile__flipper"])[${i}]`)
                             .waitForElementVisible(`(//*[@class="c-shopify-product-tile"])[${i}]/*[@class="c-shopify-product-tile__flipper c-shopify-product-tile__flipper--flipped"]`,1000)
                             .click(`(//*[@class="c-shopify-product-tile"])[${i}]/*[@class="c-shopify-product-tile__flipper c-shopify-product-tile__flipper--flipped"]`)
@@ -22,33 +23,46 @@ module.exports=(browser)=>{
                                 var seriousEatsHandleID=handleID.value[0]
                                 browser
                                     .switchWindow(amazonHandleID)
-                                    .getText('//*[@id="productTitle"]',amazonTitle=>{
-                                        var productTitle=amazonTitle.value
-                                        var searchResult=productTitle.search(productRegExp)
-                                        if(searchResult!==-1){
+                                    .api.url(shopUrl=>{
+                                        var shopUrlText=shopUrl.value
+                                        var shopUrlCheck=shopUrlText.search(productRegExp)
+                                        if(shopUrlCheck!==-1){
                                             console.log('Search match')
                                         }
-                                        else if(searchResult==-1){
+                                        else{
                                             browser
-                                                .getText('//*[@id="feature-bullets"]',featureText=>{
-                                            var featureSearch=featureText.value.search(productRegExp)
-                                            if(featureSearch!==-1){
-                                                console.log('Search match')
-                                            }
-                                            else{
-                                        console.log('No search match')
-                                            }
-                                            })
+                                                .getText('//*[@id="productTitle"]',amazonTitle=>{
+                                                    var productTitle=amazonTitle.value
+                                                    var searchResult=productTitle.search(productRegExp)
+                                                    if(searchResult!==-1){
+                                                        console.log('Search match')
+                                                    }
+                                                    else if(searchResult==-1){
+                                                        browser
+                                                            .getText('//*[@id="feature-bullets"]',featureText=>{
+                                                        var featureSearch=featureText.value.search(productRegExp)
+                                                        if(featureSearch!==-1){
+                                                            console.log('Search match')
+                                                        }
+                                                        else{
+                                                    console.log('No search match')
+                                                        }
+                                                        })
+                                                    }
+            
+                
+            
+                                                })
+        
+
                                         }
-
-                                        browser
-                                            .closeWindow()
-                                            .switchWindow(seriousEatsHandleID)
-    
-
                                     })
+                                browser
+                                    .closeWindow()
+                                    .switchWindow(seriousEatsHandleID)
 
                             })
                     })
+                
             }
 }
